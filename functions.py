5fbf9f684e8afef5a3d7118777d814b22aa3aa93
Bartosz Sobocki <33702834+bsobocki@users.noreply.github.com>
2019-10-29 18:52:36 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,9 @@
-# freeze member
-def freeze(cursor, member):
+def freeze_member(cursor, member):
     cursor.execute('UPDATE member SET active = FALSE WHERE id =' + str(member))
 
 def insert_test_values(cursor):
     cursor.execute(open('sql/test.sql').read())
 
-# returns table with all trolls
 def trolls(cursor):
     cursor.execute(open('sql/trolls.sql').read())
     records = cursor.fetchall()
@@ -14,7 +12,6 @@ def trolls(cursor):
         records[i][0] = int(records[i][0])
     return records
 
-# insert value to the database
 def insert(cursor, table, *vals):
     s = 'INSERT INTO ' + table + ' VALUES('
     for i in range(0,len(vals)):
@@ -24,12 +21,11 @@ def insert(cursor, table, *vals):
     s += ');'
     cursor.execute(s)
 
-# update value in rh database
 def update(cursor, table, set, where):
     s = 'UPDATE ' + str(table) + ' SET '+ str(set) + ' WHERE ' + str(where)
     cursor.execute(s)
 
-def query(cursor, q):
+def get_val(cursor, q):
     cursor.execute(q)
     l = cursor.fetchone()
     if l == None :
@@ -38,20 +34,19 @@ def query(cursor, q):
         return l[0]
     return None
 
-# check if member exists in table 'member'
 def member_exists(cursor, member):
     q1 = '(SELECT id FROM member)'
     q = 'SELECT (' + str(member) + ' IN ' + q1 + ');'
-    return query(cursor, q)
+    return get_val(cursor, q)
 
 def member_password(cursor, member, password):
     q = 'SELECT id FROM member WHERE id = ' + str(member) + " AND password = crypt('" + password +"', password)" 
-    return query(cursor, q)
+    return get_val(cursor, q)
 
 def member_last_timestamp(cursor, member):
     q = 'SELECT last_timestamp FROM member WHERE id = ' + str(member)
-    return query(cursor, q)
+    return get_val(cursor, q)
 
 def member_leader(cursor, member):
     q = 'SELECT leader FROM member WHERE id = ' + str(member)
-    return query(cursor, q)
+    return get_val(cursor, q)
